5f1d0c44c852ce5955e8286e9a6db1790bf3d8d5
mrodz <79176075+mrodz@users.noreply.github.com>
2023-08-17 17:48:36 -0700

    Initialize repo contents

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,88 @@
+<script>
+	import "../app.sass";
+
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
+
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+
+	export const prerender = true;
+</script>
+
+<AppShell class="h-full">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<nav class="list-nav">
+					<ul style="display: inline-block">
+						<li style="display: inline-block">
+							<a href="/account">
+								<span class="badge bg-primary-500">💀</span>
+								<span class="flex-auto">Services</span>
+							</a>
+						</li>
+						<li style="display: inline-block">
+							<a href="/account">
+								<span class="badge bg-primary-500">💀</span>
+								<span class="flex-auto">About Us</span>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<Avatar initials="JD" border="border-4 border-surface-300-600-token hover:!border-primary-900" cursor="cursor-pointer" background="bg-primary-500" rounded="rounded-xl" width="w-16" />
+			</svelte:fragment>
+		</AppBar>
+		<!--
+			<ul>
+				<li>
+					
+				</li>
+				<li>
+				</li>
+			</ul>
+		</nav>-->
+	</svelte:fragment>
+	<slot />
+</AppShell>
+<!--
+<div id="root">
+	<nav>
+		<button>Home</button>
+		<button>About</button>
+		<button>Pricing</button>
+		<Avatar initials="JD" />
+	</nav>
+  	<main>
+		<slot />
+  </main>
+</div>
+-->
+<style lang="sass">
+	// #root
+	// 	display: grid
+	// 	grid-template-rows: auto 1fr
+	// 	min-height: 100vh
+
+	// nav
+	// 	display: flex
+	// 	flex-direction: row
+	// 	background-color: lime
+	// 	position: relative
+
+	// 	&>button:last-child
+	// 		position: absolute
+	// 		right: 0
+
+	// button
+	// 	background-color: transparent
+	// 	border: none
+	// 	padding: 20px
+</style>
---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,112 @@
+<svelte:head>
+	<title>Sign In To Continue</title>
+</svelte:head>
+
+<div id="login-page" class="flex-1">
+	<div id="logo-desktop">
+		<div id="logo-desktop-pill"></div>
+		<h2 class="h2">Fieldz</h2>
+		<img src="/assets/logo.svg" width="110px" height="110px" alt=""/>
+	</div>
+	<div class="card p-4 mr-10">
+		<img id="logo-mobile" src="/assets/logo.svg" width="100px" height="100px" alt=""/>
+		<h1 class="h1">Sign In <span id="logo-mobile">To Fieldz</span></h1>
+
+		<hr class="!border-t-4 my-5" />
+
+		
+
+		<label class="label">
+			<span>Username &mdash;</span>
+			<input class="input variant-form-material  w-1/3" type="text" placeholder="Start typing a username here" />
+		</label>
+
+		<label class="label">
+			<span>Password &mdash;</span>
+			<input class="input variant-form-material  w-1/3" type="text" placeholder="Start typing your password here" />
+		</label>
+	</div>
+</div>
+
+<style lang="sass">
+	$yellow: #e3c21e
+	$yellow-transparent: #e3c21e88
+	#login-page
+		background-color: #fff
+		box-sizing: border-box
+		height: 100%
+		display: grid
+
+		padding: 50px 0
+
+		// we don't need a fallback because this image is cosmetic
+		background-image: -webkit-linear-gradient(left, rgba(255,255,255,.2) 0%, rgba(255,255,255,.7) 20%, rgba(255,255,255,1) 30%, rgba(255,255,255,1) 100%), url(assets/grass.jpg)
+
+
+		// padding-top: 50px
+		// padding-bottom: 50px
+		// padding-right: 15%
+
+		grid-template-columns: 30% 70%
+
+	#logo-desktop-pill
+		background-color: $yellow
+		opacity: .3
+		width: 100%
+		height: 100%
+
+		box-sizing: border-box
+
+		border-radius: 100vmax
+		position: absolute
+		top: 0
+		left: 0 
+
+		animation: pill-pulse infinite 5s
+
+	#logo-desktop
+		position: relative
+		margin: auto
+		text-align: center
+		box-sizing: unset
+		padding: 35px
+		// padding: 50px 50px 7.5% 50px
+		max-width: 100px
+
+		& > img
+			margin: 25px 0px
+
+		& > *:not(#logo-desktop-pill)
+			position: relative
+			z-index: 2
+
+	h2
+		font-size: 24pt
+
+	h1
+		margin: 0
+		color: $yellow
+
+	#logo-mobile
+		display: none
+
+	
+	@keyframes pill-pulse
+		0%
+			scale: 1
+		50%
+			scale: 1.25
+		100%
+			scale: 1
+
+	@media screen and (max-width: 900px)
+		#login-page
+			grid-template-columns: 1fr
+		#logo-desktop
+			display: none
+		#logo-mobile
+			display: unset
+			width: 100%
+		#logo-mobile,h1
+			text-align: center
+</style>
