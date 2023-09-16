d59c5ca2e05324855a418a5665933229c14ae3b6
mrodz <79176075+mrodz@users.noreply.github.com>
2023-09-15 19:10:41 -0700

    first vercel deploy

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <h1 class="h1">Developer Map</h1>
 
+<h2>VERCEL DEPLOYMENT!</h2>
+
 <a class="btn variant-filled bg-red-600" href="/login">Test Login/Signup</a>
 <a class="btn variant-filled bg-yellow-600" href="/about">View Sample Mission Statement</a>
