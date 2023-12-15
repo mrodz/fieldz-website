014e6b073d1b2da5280b46b1bb12bb1e0d40b88a
Mateo Rodriguez <mateotrodriguez@gmail.com>
2023-12-15 11:48:49 -0800

    add loading screen

--- frontend/src/routes/profile/+layout.svelte
+++ frontend/src/routes/profile/+layout.svelte
@@ -1,95 +1,135 @@
 <script lang="ts">
 	// import { Auth } from 'aws-amplify';
-	import { toastStore } from '@skeletonlabs/skeleton';
-	import { goto } from '$app/navigation';
-	import { currentUser } from '$lib';
+	import { toastStore } from "@skeletonlabs/skeleton";
+	import { goto } from "$app/navigation";
+	import { currentUser } from "$lib";
 	// import { setContext} from 'svelte';
-	import { ProgressRadial } from '@skeletonlabs/skeleton';
+	import { ProgressRadial } from "@skeletonlabs/skeleton";
+	import { onDestroy, onMount } from "svelte";
+	import Page from "../+page.svelte";
 
 	const err = (message: string) => {
 		toastStore.trigger({
 			message,
-			background: 'variant-filled-error',
+			background: "variant-filled-error",
 		});
 		goto("/login");
-	}
+	};
 
 	let toastId: string | undefined;
 
 	$: if ($currentUser !== undefined) {
-		$currentUser.then(user => {
-			if (user === undefined) {
-				err("You're not signed in!");
-			}
+		$currentUser
+			.then((user) => {
+				if (user === undefined) {
+					err("You're not signed in!");
+				}
 
-			if (toastId !== undefined) {
-				toastStore.close(toastId);
-				toastId = undefined;
+				if (toastId !== undefined) {
+					toastStore.close(toastId);
+					toastId = undefined;
 
-				toastStore.trigger({
-					message: "Logged in!",
-					background: "variant-filled-success",
-				})
-			}
-		}).catch(error => {
-			// this message is handled separately
-			if (error !== "The user is not authenticated") {
-				err(error);
-			}
+					toastStore.trigger({
+						message: "Logged in!",
+						background: "variant-filled-success",
+					});
+				}
+			})
+			.catch((error) => {
+				// this message is handled separately
+				if (error !== "The user is not authenticated") {
+					err(error);
+				}
 
-			console.error(error);
-		});
+				console.error(error);
+			});
 	} else {
 		/*
 		Server Side
 		*/
-		
 		toastId = toastStore.trigger({
 			message: "Please sign in to continue",
-			background: 'variant-filled-error',
+			background: "variant-filled-error",
 			hideDismiss: true,
 			timeout: 7_000,
 		});
 
 		console.log(`$currentUser is ${$currentUser} when testing for auth`);
 	}
+
+	let waiting = true;
+
+	let timeout: NodeJS.Timeout;
+
+	onMount(() => {
+		timeout = setTimeout(() => {
+			waiting = false;
+		}, 4_000);
+	});
+
+	onDestroy(() => timeout !== undefined && clearTimeout(timeout));
 </script>
 
 {#if $currentUser === undefined}
 	<div class="flex flex-col place-items-center">
 		<h1 class="h1 my-5">Please wait</h1>
 		<p class="mb-5">Awaiting a user session to proceed...</p>
-	
+
 		<ProgressRadial />
 
-		<p class="mt-5">Stuck? Try <a class="btn variant-filled" href="/login">Signing In</a></p>
+		<p class="mt-5">
+			Stuck? Try <a class="btn variant-filled" href="/login">Signing In</a
+			>
+		</p>
 	</div>
 {:else}
 	{#await $currentUser}
 		<div class="flex flex-col place-items-center">
 			<h1 class="h1 my-5">Please wait</h1>
 			<p class="mb-5">Resolving request...</p>
-	
+
 			<ProgressRadial />
 
 			<p class="mt-5">Almost there!</p>
 		</div>
-	{:then user}
+	{:then}
 		<slot />
 	{:catch error}
-		<div class="p-4">
-			<h1 class="h1 my-5">Sorry! You can't view this page.</h1>
+		{#if waiting}
+			<div class="flex flex-col place-items-center">
+				<h1 class="h1 my-5">Please wait</h1>
+				<p class="mb-5">Resolving request...</p>
 
-			<p>It looks like you don't have permission to access this route. Try signing in again!</p>
+				<ProgressRadial />
 
-			<p class="my-5">If the problem persists, please email us for support at <a href="mailto:admin@fieldz.app">admin@fieldz.app</a>.</p>
+				<p class="mt-5">Thinking ...</p>
+			</div>
+		{:else}
+			<div class="p-4">
+				<h1 class="h1 my-5">Sorry! You can't view this page.</h1>
 
-			<p>You will be redirected shortly. If not, click <button class="btn variant-filled">here</button></p>
+				<p>
+					It looks like you don't have permission to access this
+					route. Try signing in again!
+				</p>
 
-			<h2>Raw Error:</h2>
-			<code>
-				{JSON.stringify(error)}
-			</code>
-		</div>
+				<p class="my-5">
+					If the problem persists, please email us for support at <a
+						href="mailto:admin@fieldz.app">admin@fieldz.app</a
+					>.
+				</p>
+
+				<p>
+					You will be redirected shortly. If not, click <button
+						class="btn variant-filled">here</button
+					>
+				</p>
+
+				<h2>Raw Error:</h2>
+				<code>
+					{JSON.stringify(error)}
+				</code>
+			</div>
+		{/if}
 	{/await}
 {/if}
