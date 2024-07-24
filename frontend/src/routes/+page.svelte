b335a2178920123c8276c6fd93318c017cabd1de
mrodz <79176075+mrodz@users.noreply.github.com>
2024-07-23 17:19:44 -0700

    Update services page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { blur, fade } from "svelte/transition";
+  import { blur, fade, slide } from "svelte/transition";
   import markdownit from "markdown-it";
 
   const md = markdownit();
@@ -57,108 +57,115 @@
   <title>Fieldz</title>
 </svelte:head>
 
-<section id="landing-graphic">
-  <div class="my-auto flex flex-col items-center box-border">
-    {#if ready}
-      <h1 in:blur={{ duration: 1000 }} class="h1">Fieldz</h1>
-    {/if}
-    <span class="typewriter">Youth Sports' Best Friend</span>
-  </div>
-</section>
-
-<section>
-  <div id="screenshots" class="grid grid-cols-11 grid-rows-11 my-8">
-    {#each screenshots as screenshot, i (screenshot.url)}
-      <img
-        in:fade={{ delay: i * 100, duration: 250 }}
-        class="app-screenshot"
-        src={screenshot.url}
-        alt={screenshot.alt}
-      />
-    {/each}
-
-    <div class="p-8">
-      <section class="card h-full p-8">
-        <h2 class="h2 text-center md:text-left">
-          Welcome to a New Era of Sports Management
-        </h2>
-        <p class="my-4 xl:text-2xl text-center md:text-left">
-          Fieldz is a web app that aims to help managers of youth sports leagues
-          efficiently manage their coaches, referee bookings, and team drafting.
-        </p>
-        <p
-          id="expected-release-text"
-          class="my-4 xl:text-2xl text-center md:text-left"
-        >
-          While this app is still in its early stages of development, it is
-          scheduled for use by real coaches in August 2024.
-        </p>
-      </section>
+<main>
+  <section id="landing-graphic">
+    <div class="my-auto flex flex-col items-center box-border">
+      {#if ready}
+        <h1 in:blur={{ duration: 1000 }} class="h1">Fieldz</h1>
+      {/if}
+      <span class="typewriter">Youth Sports' Best Friend</span>
     </div>
-  </div>
-</section>
+  </section>
 
-<hr class="max-[400px]:!hidden" />
+  <section in:fade={{}}>
+    <div id="screenshots" class="grid grid-cols-11 grid-rows-11 my-8">
+      {#each screenshots as screenshot, i (screenshot.url)}
+        {#if ready}
+          <img
+            in:fade={{ delay: i * 750, duration: 750 }}
+            class="app-screenshot"
+            src={screenshot.url}
+            alt={screenshot.alt}
+          />
+        {/if}
+      {/each}
 
-<section class="mx-10 flex flex-col md:block">
-  <h2 class="h2 mb-5 text-center md:text-left">Free & Open</h2>
-  <p class="xl:text-2xl text-center md:text-left">
-    Fieldz is an open-source application, which means that you can find all of
-    the app's source code online! Everything, which includes the desktop
-    application, backend code, database schema, and cloud functions are
-    available under an MPL-2.0 license. In fact, you can view all of the app's
-    code
-
-    <a
-      class="link underline hover:text-primary-500"
-      href="https://github.com/mrodz/fieldz-desktop"
-      target="blank"
-      rel="noopener noreferrer">here</a
-    >.
-
-    <br />
-    <br />
-
-    I intend to keep this app free for as long as possible, relying entirely on
-    donations to fund deployment costs, hosting, and cloud charges.
-  </p>
-</section>
-
-<section class="mx-10 flex flex-col md:block pb-10">
-  <h2 class="h2 mb-5 text-center md:text-left">General Progress</h2>
-  {#if releaseData !== undefined}
-    {#await releaseData}
-      Loading release data...
-    {:then releaseData}
-      {#if releaseData === undefined || "message" in releaseData}
-        <a
-          class="mx-4 link underline hover:text-primary-500"
-          href="https://github.com/mrodz/fieldz-desktop/releases/latest"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          See latest release here
-        </a>
-      {:else}
-        <a
-          class="mx-4"
-          href="https://github.com/mrodz/fieldz-desktop/releases/latest"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span class="chip variant-filled">Latest Release</span>
-          {releaseData.name}
-        </a>
-
-        <div class="card m-4 p-4">
-          {@html md.render(releaseData.body)}
+      {#if ready}
+        <div class="p-8" in:slide={{ axis: "y" }}>
+          <section class="card h-full p-8">
+            <h2 class="h2 text-center md:text-left">
+              Welcome to a New Era of Sports Management
+            </h2>
+            <p class="my-4 xl:text-2xl text-center md:text-left">
+              Fieldz is a web app that aims to help managers of youth sports
+              leagues efficiently manage their coaches, referee bookings, and
+              team drafting.
+            </p>
+            <p
+              id="expected-release-text"
+              class="my-4 xl:text-2xl text-center md:text-left"
+            >
+              While this app is still in its early stages of development, it is
+              scheduled for use by real coaches in August 2024.
+            </p>
+          </section>
         </div>
       {/if}
-    {:catch err}
-      {JSON.stringify(err)}
-    {/await}
-  {/if}
-</section>
+    </div>
+  </section>
+
+  <hr class="max-[400px]:!hidden" />
+
+  <section class="mx-10 flex flex-col md:block">
+    <h2 class="h2 mb-5 text-center md:text-left">Free & Open</h2>
+    <p class="xl:text-2xl text-center md:text-left">
+      Fieldz is an open-source application, which means that you can find all of
+      the app's source code online! Everything, which includes the desktop
+      application, backend code, database schema, and cloud functions are
+      available under an MPL-2.0 license. In fact, you can view all of the app's
+      code
+
+      <a
+        class="link underline hover:text-primary-500"
+        href="https://github.com/mrodz/fieldz-desktop"
+        target="blank"
+        rel="noopener noreferrer">here</a
+      >.
+
+      <br />
+      <br />
+
+      I intend to keep this app free for as long as possible, relying entirely
+      on donations to fund deployment costs, hosting, and cloud charges.
+    </p>
+  </section>
+
+  <section class="mx-10 flex flex-col md:block pb-10">
+    <h2 class="h2 mb-5 text-center md:text-left">General Progress</h2>
+    {#if releaseData !== undefined}
+      {#await releaseData}
+        Loading release data...
+      {:then releaseData}
+        {#if releaseData === undefined || "message" in releaseData}
+          <a
+            class="mx-4 link underline hover:text-primary-500"
+            href="https://github.com/mrodz/fieldz-desktop/releases/latest"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            See latest release here
+          </a>
+        {:else}
+          <a
+            class="mx-4"
+            href="https://github.com/mrodz/fieldz-desktop/releases/latest"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span class="chip variant-filled">Latest Release</span>
+            {releaseData.name}
+          </a>
+
+          <div class="card m-4 p-4">
+            {@html md.render(releaseData.body)}
+          </div>
+        {/if}
+      {:catch err}
+        {JSON.stringify(err)}
+      {/await}
+    {/if}
+  </section>
+</main>
 
 <style lang="sass">
 
--- frontend/src/routes/products/+page.svelte
+++ frontend/src/routes/products/+page.svelte
@@ -1,7 +1,100 @@
 <script>
-	import { slide } from "svelte/transition";
+  import { slide } from "svelte/transition";
 </script>
 
-<div in:slide out:slide>
-	Insert Products Later
-</div>
+<main class="m-8" in:slide out:slide>
+  <h1 class="h1">Services</h1>
+
+  <div class="card my-4 p-4">
+    <h2 class="h2 mb-2">Fieldz Desktop (FREE)</h2>
+
+    <p>
+      Our Youth Sports Scheduling App is designed to simplify the intricate
+      process of organizing and managing youth sports events across various
+      regions. Available on Mac, Windows, and Ubuntu, this versatile application
+      caters to the needs of coaches and regional administrators, ensuring
+      seamless scheduling and coordination.
+    </p>
+
+    <hr class="hr my-4" />
+
+    <h3 class="h3">Key Features</h3>
+    <ul class="my-4">
+      <li>
+        <strong>Regional Coordination:</strong>
+        Manage multiple teams and sports events across different regions with ease.
+        The app supports region-specific settings and preferences, making it adaptable
+        to various locales.
+      </li>
+      <li>
+        <strong>User-Friendly Interface:</strong>
+        A detailed and straightforward user interface ensures that even non-technical
+        users can navigate and utilize the app efficiently.
+      </li>
+      <li>
+        <strong>Automated Scheduling:</strong>
+        Save time and reduce errors with automated scheduling algorithms that consider
+        factors such as team availability, travel distances, and venue constraints.
+      </li>
+      <li>
+        <strong> Conflict Resolution: </strong>
+        Intelligent conflict resolution tools help identify and address scheduling
+        conflicts, ensuring smooth and hassle-free planning.
+      </li>
+    </ul>
+
+    <p>
+      Whether you're organizing a local tournament or managing a league across
+      multiple regions, our Youth Sports Scheduling App provides the tools you
+      need to streamline the scheduling process, enhance communication, and
+      ensure a successful sporting season.
+    </p>
+  </div>
+
+  <div class="card my-4 p-4">
+    <h2 class="h2 mb-2">Fieldz Scheduling Credits (FREE*)</h2>
+
+    <p>
+      While the desktop client is free to install and use, sending your data to
+      our server and running our algorithm on your inputs costs us (Fieldz)
+      money.
+      <strong class="text-green-600 underline">
+        For now, this service is 100% free for all use
+      </strong>
+      , as the user-base is small. However, this is subject to change if we experience
+      a surge of users in order to facilitate the higher volume of requests.
+    </p>
+
+    <hr class="hr my-4" />
+
+    <h3 class="h3">Key Features</h3>
+    <ul class="my-4">
+      <li>
+        <strong>Pay-As-You-Go Pricing:</strong>
+        Instead of paying for a license or subscription to use our servers, you will
+        be charged only for the schedules you create on our backend. Given that our
+        app is open source, you can run the backend yourself and skip paying for
+        credits entirely. We ask for you understanding as using the cloud is not
+        free, and credits act as a medium for your region to enjoy the speed and
+        efficacy of cloud computing without the hassle of setting up the backend
+        yourself.
+      </li>
+      <li>
+        <strong>Supports Development:</strong>
+        This project was created to solve a problem in a local community, and is
+        not funded by any source. Any and all credits purchased will go towards expanding
+        the resources allocated to scheduling and our operation!
+      </li>
+    </ul>
+
+    <div class="grid grid-cols-[1fr_auto] gap-2">
+      <span class="text-red-500">*</span>
+      <p>
+        The price of scheduling credits is subject to change. Any Fieldz
+        accounts will be notified before changes take place and the price of
+        creating a schedule will resemble the market price of all cloud compute
+        resources allocated to create the schedule.
+      </p>
+    </div>
+  </div>
+</main>
